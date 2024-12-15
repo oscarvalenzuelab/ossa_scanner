cbfea32b8754ac26d23a38850be7ea67714b5759
Oscar V <oscar.valenzuela.b@gmail.com>
2024-12-14 22:48:12 -0800

    cleaning

--- ossa/scanner.py
+++ ossa/scanner.py
@@ -1,11 +1,11 @@
 import os
 import json
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from ossa_scanner.utils.os_detection import detect_os
-from ossa_scanner.utils.package_manager import list_packages, get_package_info, download_source
-from ossa_scanner.utils.hash_calculator import calculate_file_hash
-from ossa_scanner.utils.swhid_calculator import calculate_swhid
-from ossa_scanner.uploader import GitHubUploader
+from ossa.utils.os_detection import detect_os
+from ossa.utils.package_manager import list_packages, get_package_info, download_source
+from ossa.utils.hash_calculator import calculate_file_hash
+from ossa.utils.swhid_calculator import calculate_swhid
+from ossa.uploader import GitHubUploader
 
 class Scanner:
     def __init__(self, output_dir, threads=4):
