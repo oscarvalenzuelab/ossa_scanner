1d6f2f2113eedd778dfc9f08bc2503a74db5c8dc
Oscar V <oscar.valenzuela.b@gmail.com>
2024-12-15 16:31:27 -0800

    fixing brew

--- ossa_scanner/utils/downloader.py
+++ ossa_scanner/utils/downloader.py
@@ -30,6 +30,7 @@ def download_source(package_manager, package_name, output_dir):
                 print(f"Source package for {package_name} not found in {package_name}.")
                 return
             spec_file = extract_rpm_spec_file(source_path, output_dir)
+            print('spec_file:', spec_file)
             project_url, source_url = (None, None)
             if spec_file:
                 project_url, source_url, license = extract_rpm_info_from_spec(spec_file)
