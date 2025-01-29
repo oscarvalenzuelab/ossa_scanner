4cd382ff31b39ad34bd014ba77b19f35fea03840
Oscar V <oscar.valenzuela.b@gmail.com>
2025-01-29 07:42:01 -0800

    small improvements

--- ossa_scanner/utils/os_detection.py
+++ ossa_scanner/utils/os_detection.py
@@ -10,7 +10,7 @@ def detect_pm():
     dist = distro.id()
     if 'ubuntu' in dist or 'debian' in dist:
         return 'apt'
-    elif 'redhat' in dist or 'centos' in dist or 'almalinux' in dist:
+    elif 'redhat' in dist or 'centos' in dist or 'almalinux' in dist or 'amzn' in dist:
         return 'yum'
     elif 'darwin' in dist:
         return 'brew'
